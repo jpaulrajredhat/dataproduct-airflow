3f4349787afea471646125bded10fd162a161261
Jey Paulraj <85969025+jpaulrajredhat@users.noreply.github.com>
2026-05-19 08:53:37 -0500

    Update single_mortgage_delinquent_pipeline.py

--- dags/single_mortgage_delinquent_pipeline.py
+++ dags/single_mortgage_delinquent_pipeline.py
@@ -37,7 +37,7 @@ OAUTH_CLIENT_ID = os.environ.get("OAUTH_CLIENT_ID", "trino")
 OAUTH_CLIENT_SECRET = os.environ.get("OAUTH_CLIENT_SECRET", "EyxK5moeRzprovZYrv2ENUUK4Ur7pfA6")
 OAUTH_USER_NAME = os.environ.get("OAUTH_USER_NAME", "admin")
 OAUTH_PASSWORD = os.environ.get("OAUTH_PASSWORD", "Redhat2026$")
-TRINO_HOST = os.environ.get("TRINO_HOST", "trino-datamesh.apps.ocp.zsc6x.sandbox877.opentlc.com")
+TRINO_HOST = os.environ.get("TRINO_HOST", "trino.datamesh.svc.cluster.local")
 
 print("DEBUG OAUTH_URL-1:", OAUTH_URL)
     
